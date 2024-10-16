0855ed375b2d9b0ee2724449fdf9293f406c20f6
niko <nikolaguemmoud@hotmail.fr>
2024-10-16 17:14:54 +0200

    premiere mise a jour

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once(__DIR__."/src/common/bdd.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,18 @@
     <title>Shop fruitz</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-    
+    <div class="titre">
+        <h1>Fruitz Shop</h1>
+    </div>
+    <div class="catalogue">
+        <?php for ($i=0; $i < count($fruits); $i++) :?>
+            <p class="produit"><a href="src/product/product.php?id=<?=$i?>">produit <?=$i?></a> </p> 
+        <?php endfor;?>
+    </div>
 </body>
 </html>
--- src/product/product.php
+++ src/product/product.php
@@ -1,4 +1,6 @@
 <?php
+require_once(__DIR__."/../common/bdd.php");
+$produits_id=$_GET["id"];
 
 ?>
 
@@ -7,11 +9,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Produit</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../style.css">
     <link rel="stylesheet" href="product.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-    
+    <h1> Page produit</h1>
+    <p><?= $fruits[$produits_id]["name"] ?></p>
+    <a href="../../index.php">accueil</a>
 </body>
 </html>
